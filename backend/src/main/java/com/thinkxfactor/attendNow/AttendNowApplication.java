0f0a61c0a2c14618ef07db6ffb932997478aa42c
Mriganka Manna <mrigankamanna3@gmail.com>
2024-05-11 17:24:22 +0530

    Add new rest controller api

--- backend/src/main/java/com/thinkxfactor/attendNow/AttendNowApplication.java
+++ backend/src/main/java/com/thinkxfactor/attendNow/AttendNowApplication.java
@@ -2,6 +2,9 @@ package com.thinkxfactor.attendNow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AttendNowApplication {
@@ -10,4 +13,15 @@ public class AttendNowApplication {
 		SpringApplication.run(AttendNowApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/api")
+class TestController{
+
+	@GetMapping("/hello")
+	public String hello(){
+		System.out.println("Welcome to Hello World API");
+		return "Hello, World from controllers!!";
+	}
 }
